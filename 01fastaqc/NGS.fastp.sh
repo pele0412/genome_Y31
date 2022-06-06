3f59e3fd6a358683958fc09e268152c03a584ac4
pele0412 <58616133+pele0412@users.noreply.github.com>
2022-06-06 15:36:08 +0800

    Update NGS.fastp.sh

--- 01fastaqc/NGS.fastp.sh
+++ 01fastaqc/NGS.fastp.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
-fastp -i 31_raw_R1.fastq.gz -I 31_raw_R2.fastq.gz \
-    -o 31_filter_R1.fastq.gz -O 31_filter_R2.fastq.gz \
-    -w 16 -h Y31.html -j Y31.json \
+p1=/home/pele/analysis/genome_assembly/raw_data/Y31/NGS/Y31_1.fq.gz
+p2=/home/pele/analysis/genome_assembly/raw_data/Y31/NGS/Y31_2.fq.gz
+p3=31_NGS_clean_R1.fastq.gz
+p4=31_NGS_clean_R2.fastq.gz
+
+fastp -i $p1 -I $p2 \
+    -o $p3 -O $p4 \
+    -w 16 -h Y31.NGS.html -j Y31.NGS.json \
     -c -l 80 -5 -3 -W 4 -M 20 --adapter_fasta adapter.fasta \
-    -f 13 -F 13 -t 3 -T 3 \
+    -f 10 -F 13 -t 3 -T 3 \
     -q 20 -u 40

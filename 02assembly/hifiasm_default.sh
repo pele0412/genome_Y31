b1e7459ac9730175ba794a13c6be612b584088f4
pele0412 <58616133+pele0412@users.noreply.github.com>
2022-06-08 08:26:25 +0800

    Update hifiasm_default.sh

--- 02assembly/hifiasm_default.sh
+++ 02assembly/hifiasm_default.sh
@@ -1,1 +1,2 @@
 hifiasm -o Y31.asm -t 50 ~/analysis/genome_assembly/01fastaqc/hifi/Y31.hifi.clean.fastq.gz
+nohup hifiasm -primary -o Y31.asm -t 50 ~/analysis/genome_assembly/01fastaqc/hifi/Y31.hifi.clean.fastq.gz 2> Y31.log &

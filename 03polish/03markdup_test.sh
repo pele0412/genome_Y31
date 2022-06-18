015ef72ea0405172819668757fffd69b96fa9d0a
pele0412 <58616133+pele0412@users.noreply.github.com>
2022-06-18 09:55:31 +0800

    Update 03markdup_test.sh

--- 03polish/03markdup_test.sh
+++ 03polish/03markdup_test.sh
@@ -1,1 +1,2 @@
 nohup java -Djava.io.tmpdir=tmp -Xmx40g -jar ~/biotools/picard/picard.jar MarkDuplicates INPUT=others_contig.fasta.sort.uniqe_q30.bam OUTPUT=others_contig.fasta.sort.uniqe_q30.mark.bam METRICS_FILE=others_contig.fasta.metric CREATE_INDEX=ture REMOVE_DUPLICATES=ture &
+nohup java -Djava.io.tmpdir=tmp -Xmx40g -jar ~/biotools/picard/picard.jar MarkDuplicates INPUT=others_contig.fasta.sort.uniqe_q30.bam OUTPUT=t1.bam METRICS_FILE=t1.metric CREATE_INDEX=ture 2> t1.log &

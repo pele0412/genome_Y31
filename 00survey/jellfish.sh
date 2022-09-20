c3e05f8915a199f761351ae0ae12a1616a6d21a3
pele0412 <58616133+pele0412@users.noreply.github.com>
2022-09-20 08:59:15 +0800

    Update jellfish.sh

--- 00survey/jellfish.sh
+++ 00survey/jellfish.sh
@@ -1,1 +1,3 @@
 nohup jellyfish count -m 21 -s 4G -C -t 10 -o 21mer_out <(zcat Y31.filter_1.fq.gz) <(zcat Y31.filter_2.fq.gz) &
+nohup jellyfish stats 21mer_out > 21-k-mer.stats &
+nohup jellyfish histo -t 100 -o 21-mer.hist 21mer_out &
